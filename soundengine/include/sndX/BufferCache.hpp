8dd5a9125f0853203ba2eecbad1959bc73868fa2
psy_inf <sebastian@smesserschmidt.de>
2025-01-12 18:00:55 +0100

    BufferCache with delegates

--- soundengine/include/sndX/BufferCache.hpp
+++ soundengine/include/sndX/BufferCache.hpp
@@ -8,33 +8,77 @@
 
 namespace soundEngineX {
 
+/* Mono-state that allows to "get" resources based on a filename, stream or memory buffer.
+ * The cache will load the resource if it is not already loaded.
+ * The cache will use the provided delegates to load the resource if they are set.
+ * If the delegates are not set, the cache will use the default loader.
+ */
+
 class BufferCache
 {
 public:
+    using StreamLoaderDelegate =
+        std::function<std::shared_ptr<soundEngineX::Buffer>(std::istream&,
+                                                            soundEngineX::loader::Type,
+                                                            soundEngineX::loader::LoadingCallback)>;
+    using FileLoaderDelegate =
+        std::function<std::shared_ptr<soundEngineX::Buffer>(const std::string&, soundEngineX::loader::LoadingCallback)>;
+
+    // TODO: this should be a std::span
+    using MemBufLoaderDelegate =
+        std::function<std::shared_ptr<soundEngineX::Buffer>(const std::vector<char>&,
+                                                            soundEngineX::loader::Type,
+                                                            soundEngineX::loader::LoadingCallback)>;
+
     static bool has(const std::string& filename) { return buffers.find(filename) != buffers.end(); }
 
-    static std::shared_ptr<soundEngineX::Buffer>& get(const std::string&                    filename,
+    static std::shared_ptr<soundEngineX::Buffer>& get(const std::string&                    key,
+                                                      FileLoaderDelegate                    file_loader_delegate = {},
                                                       soundEngineX::loader::LoadingCallback progress_cb = {})
     {
-        auto it = buffers.find(filename);
-        if (it == buffers.end())
+        if (!buffers.contains(key))
         {
-            buffers[filename] =
-                std::make_unique<soundEngineX::Buffer>(soundEngineX::loader::load(filename, progress_cb));
+            if (file_loader_delegate) { buffers[key] = file_loader_delegate(key, progress_cb); }
+            else
+            {
+                buffers[key] = std::make_unique<soundEngineX::Buffer>(soundEngineX::loader::load(key, progress_cb));
+            }
         }
-        return buffers[filename];
+        return buffers[key];
     }
 
     static std::shared_ptr<soundEngineX::Buffer>& get(const std::string&                    key,
                                                       std::istream&                         stream,
                                                       const soundEngineX::loader::Type      type,
+                                                      StreamLoaderDelegate                  stream_loader_delegate = {},
                                                       soundEngineX::loader::LoadingCallback progress_cb = {})
     {
-        auto it = buffers.find(key);
-        if (it == buffers.end())
+        if (!buffers.contains(key))
         {
-            buffers[key] =
-                std::make_unique<soundEngineX::Buffer>(soundEngineX::loader::load(stream, type, progress_cb));
+            if (stream_loader_delegate) { buffers[key] = stream_loader_delegate(stream, type, progress_cb); }
+            else
+            {
+                buffers[key] =
+                    std::make_unique<soundEngineX::Buffer>(soundEngineX::loader::load(stream, type, progress_cb));
+            }
+        }
+        return buffers[key];
+    }
+
+    static std::shared_ptr<soundEngineX::Buffer>& get(const std::string&                    key,
+                                                      const std::vector<char>&              data,
+                                                      const soundEngineX::loader::Type      type,
+                                                      MemBufLoaderDelegate                  loader_delegate = {},
+                                                      soundEngineX::loader::LoadingCallback progress_cb = {})
+    {
+        if (!buffers.contains(key))
+        {
+            if (loader_delegate) { buffers[key] = loader_delegate(data, type, progress_cb); }
+            else
+            {
+                buffers[key] =
+                    std::make_unique<soundEngineX::Buffer>(soundEngineX::loader::load(data, type, progress_cb));
+            }
         }
         return buffers[key];
     }
